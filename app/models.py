57dea3cbd859ce92050807662bb2d0241db3406c
LucasZick <lucasalexandrezick@gmail.com>
2025-10-14 19:38:59 -0300

    bigger password hash

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     logs = db.relationship('LogEntry', backref='author', lazy='dynamic')
     role = db.Column(db.String(20), index=True, default='bolsista')
     is_approved = db.Column(db.Boolean, default=False)
